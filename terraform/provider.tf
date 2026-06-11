cc551aa04f6072b5c1976d5e525be26fa1294a49
alan3002 <alanbiju3002.com>
2026-06-11 10:06:53 +0000

    Push images with v1 tag

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6"
 
+  backend "s3" {
+    bucket = "terraform-state-file-devops-capstone-2"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
