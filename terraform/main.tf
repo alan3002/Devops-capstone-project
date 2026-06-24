079a171ec6f5bf286c5ea236e2c8817f46c2e15f
alanbiju3002 <alanbiju3002@gmail.com>
2026-06-24 10:52:30 +0530

    Reformat module definitions in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,11 +24,3 @@ module "rds" {
   private_subnet_ids = module.vpc.private_subnet_ids
   db_password        = var.db_password
 }
-module "lambda_s3" {
-  source = "./modules/lambda-s3"
-
-  project_name = "devops-master"
-}
-module "loki_s3" {
-  source = "./modules/loki-s3"
-}
